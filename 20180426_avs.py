171d28e019f4c92066d75bce378dac35a85e974e
Jack Chua <jachua@BELC02RV25SG8WN.sea.corp.expecn.com>
2018-04-29 21:31:07 -0700

    avs

--- 20180426_avs.py
+++ 20180426_avs.py
@@ -5,6 +5,7 @@ import statsmodels as sm
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 
 pd.set_option('display.height', 1000)
 pd.set_option('display.max_rows', 500)
@@ -13,37 +14,53 @@ pd.set_option('display.width', 1000)
 
 # read trx data
 colnames = 'id,chain,dept,category,company,brand,date,productsize,productmeasure,purchasequantity,purchaseamount'.split(',')
-loc_dir = '/home/jack/data/acquirevaluedshoppers'
+loc_dir = '/Users/jachua/data/acquirevaluedshoppers'
 trx_loc = os.path.join(loc_dir, 'transactions_sampled.csv.gz')
 trx = pd.read_csv(trx_loc, compression='gzip', names=colnames, sep=',')
 
+# initialize encoders
+label_encoders = {}
+one_hot_encoders = {}
+
 ####################################################################
 # data engineering: remove refunds and negative amount values
+# TODO: remove outliers; will affect our regression
+# TODO: plot of quantity versus demand for a given product over time
 trx = trx.loc[trx['purchaseamount'] > 0.0, ]
 trx = trx.loc[trx['purchasequantity'] > 0.0, ]
-trx_counts = trx.groupby('id').agg({'purchaseamount':'count'})
-trx_counts = trx_counts.sort_values(by='purchaseamount', ascending=False)[0:50]
-trx_counts['id'] = trx_counts.index
-trx = pd.merge(trx, trx_counts[['id']], on='id', how='inner')
+trx['unitpurchaseamount'] = trx['purchaseamount']/trx['purchasequantity']
+
+# encode the product measure
+lbl_enc = LabelEncoder()
+trx['productmeasure'] = lbl_enc.fit_transform(trx['productmeasure'])
+label_encoders['productmeasure'] = lbl_enc
+
+# get the top 50 products by quantity
+trx_counts = trx.groupby('category').agg({'purchasequantity':'sum'})
+trx_counts = trx_counts.sort_values(by='purchasequantity', ascending=False)[0:50]
+trx_counts['category'] = trx_counts.index
+trx_counts['sum_purchasequantity'] = trx_counts['purchasequantity']
+trx = pd.merge(trx, trx_counts[['category','sum_purchasequantity']], on='category', how='inner')
 
 len(trx['date'].unique()) #513 days
 
 # distribution of trx counts per product
-prod_counts = trx[['id','chain']].groupby('id').count()[['chain']]
-prod_counts = prod_counts[prod_counts['chain']<=10]
+prod_counts = trx[['category','chain']].groupby('category').count()[['chain']]
 prod_counts.hist(bins=100)
 plt.title('Distribution of trx counts per product')
 plt.show()
 
 # price variation in a product
-price_var = trx[['id','purchaseamount', 'purchasequantity']].groupby('id').agg(
+price_var = trx[['category','purchaseamount', 'purchasequantity']].groupby('category').agg(
     {
         'purchaseamount':['mean','std'],
         'purchasequantity':['mean','std']
     }
 )
 price_var['purchaseamount','mean'].hist(bins=50)
-price_var['purchaseamount','std'].hist(bins=50)
+plt.show()
+price_var['purchaseamount','std'].plot(kind='kde')
+plt.show()
 
 # problem is - everything has a power law
 # you have to price products without much data with products that have more data
@@ -52,11 +69,21 @@ price_var['purchaseamount','std'].hist(bins=50)
 
 # convert to sparse dataframe
 # lots of dummy variables, have to know how to deal with it
-trx = pd.SparseDataFrame(trx)
-for cat_var in ['id','chain','dept','category','company','brand','productmeasure']:
-    dums = pd.get_dummies(trx[cat_var],prefix=cat_var,sparse=True)
-    trx = pd.concat([trx, dums], axis=1)
-    del trx[cat_var+['date']]
+# apart from pd.get_dummies, there are other ways to do it (like with scipy or sklearn's
+# feature vectorizers
+
+# this will make scoring much easier
+data = pd.SparseDataFrame(trx)
+cat_vars = ['dept','category','brand','productmeasure']
+
+# then accumulate all the codes and create one hot encoding
+for cat_var in cat_vars:
+    ohe = OneHotEncoder(categorical_features='all', sparse=True)
+    dums = ohe.fit_transform(data[cat_vars])
+    data = pd.concat([data, dums], axis=1)
+
+for var in cat_vars+['date','sum_purchasequantity','purchaseamount','company','chain']:
+    del data[var]
 
 ####################################################################
 # part 2: learning elasticities
@@ -66,9 +93,9 @@ for cat_var in ['id','chain','dept','category','company','brand','productmeasure
 # solution 1: nonparametric (random forest)
 #
 # produce a demand forecast
-rf = RandomForestRegressor()
-y = trx['purchasequantity']
-X = trx.loc[:, trx.columns != 'purchasequantity']
+rf = RandomForestRegressor(n_jobs=4)
+y = data['purchasequantity']
+X = data.loc[:, data.columns != 'purchasequantity']
 n_train = int(X.shape[0]*0.70)
 X_train, X_test, y_train, y_test = train_test_split(X, y,train_size=n_train,random_state=4)
 rf.fit(X_train, y_train)
@@ -83,9 +110,11 @@ mae = np.mean(np.abs(errors))
 pd.DataFrame(errors).hist(bins=50)
 plt.show()
 
-# for each product, given a rnage of prices around the product mean price (-10% to +10%), determine the best
-# price
-
-# first get all permutations of products
+# for each product, simulate the demand for a -10% to +10% increase
+# EG: what should chain 100 price for each brand of product 6320?
 price_steps = np.arange(-0.1, 0.1, 0.01)
-X[].drop_duplicates
+
+# brand=14760, category=516, chain=100
+dp = 0.05
+trx.loc[trx['category']==516,]['brand'].unique()
+
